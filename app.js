4376141c0573ae06141cdbfede7b604b2edf2592
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-09 11:54:34 +0200

    working route to get books in database

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const pool = require('./db_context');
 
 const port = 3000;
 
+// middleware
+app.use(cors());
+app.use(express.json())
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Library lady!");
+});
+
+app.get("/books", async(req, res) => {
+  try {
+    const allBooks = await pool.query('SELECT * FROM library')
+    res.json(allBooks.rows)
+  } catch (err) {
+    console.error(err)
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`API listening on port ${port}!`);
 });
