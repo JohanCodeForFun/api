a6df5c94a06811a8b21db02ddd92794602c3a4ed
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-09 11:43:12 +0200

    add server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const pool = require('./db_context');
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
