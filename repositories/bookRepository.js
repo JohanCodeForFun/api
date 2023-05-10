f363c843aa455e8a55fba5cf991163db260ec215
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-10 10:32:55 +0200

    add bookId params for Edit and format code

--- repositories/bookRepository.js
+++ repositories/bookRepository.js
@@ -1,38 +1,52 @@
-const bookModel = require('../models/bookModel');
-const db_context = require('../db_context');
+const bookModel = require("../models/bookModel");
+const db_context = require("../db_context");
 
 async function getAllBooks() {
   let books = [];
   let bookData = await db_context.selectAllBooks();
 
-  bookData.forEach(book => {
-    books.push(new bookModel(book.author, book.title, book.genre, book.published))
+  bookData.forEach((book) => {
+    books.push(
+      new bookModel(
+        book.author,
+        book.title,
+        book.genre,
+        book.published
+      )
+    );
   });
 
-  return books
+  return books;
 }
 
 async function getBookByKeyword(keyword) {
   let books = [];
-  let bookData = await db_context.selectBookByKeyword(keyword)
+  let bookData = await db_context.selectBookByKeyword(keyword);
 
-  bookData.forEach(book => {
-    books.push(new bookModel(book.author, book.title, boook.genre, boook.published))
+  bookData.forEach((book) => {
+    books.push(
+      new bookModel(
+        book.author,
+        book.title,
+        boook.genre,
+        boook.published
+      )
+    );
   });
 
-  return books
+  return books;
 }
 
 async function addBook(author, title, genre, published) {
-  db_context.insertBook(author, title, genre, published)
+  db_context.insertBook(author, title, genre, published);
 }
 
-async function editBook(author, title, genre, published) {
-  db_context.updateBook(author, title, genre, published)
+async function editBook(bookId, author, title, genre, published) {
+  db_context.updateBook(bookId, author, title, genre, published);
 }
 
-async function deleteBook(bookId) {
-  db_context.deleteBook(bookId)
+async function deleteBook(book_Id) {
+  db_context.deleteBook(book_Id);
 }
 
 module.exports = {
@@ -40,5 +54,5 @@ module.exports = {
   getBookByKeyword,
   addBook,
   editBook,
-  deleteBook
-}
+  deleteBook,
+};
