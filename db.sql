10d3163ebc647203cc46b59737a88114bd1e6534
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-12 00:08:22 +0200

    update db.sql commands

--- db.sql
+++ db.sql
@@ -1,17 +1,66 @@
-
--- library table
-CREATE TABLE library(
+-- catalogue table
+CREATE TABLE catalogue(
 	book_id SERIAL NOT NULL PRIMARY KEY,
-	author VARCHAR(100),
-	title VARCHAR(200),
-	genre VARCHAR(100),
-	published DATE
+	author VARCHAR(100) NOT NULL,
+	title VARCHAR(200) NOT NULL,
+	genre VARCHAR(50) NOT NULL,
+	published date
 )
 
--- insert record
-INSERT INTO library(
+-- patron table
+CREATE TABLE patron(
+	patron_id SERIAL NOT NULL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	joined timestamp DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE(email)
+);
+
+-- loan table
+CREATE TABLE loan(
+  loan_id SERIAL NOT NULL PRIMARY KEY,
+  patron_id int NOT NULL,
+  book_id int NOT NULL,
+  checkout_date date NOT NULL,
+  return_date date NOT NULL,
+  FOREIGN KEY (patron_id) REFERENCES patron(patron_id), 
+  FOREIGN KEY (book_id) REFERENCES catalogue(book_id), 
+);
+
+-- change name to table
+-- ALTER TABLE catalogue RENAME TO catalogue
+
+-- insert book record
+INSERT INTO catalogue(
   author,
   title,
   genre,
   published) 
   VALUES ('Robert Jordan', 'The Wheel of Time', 'Fantasy', '1999-01-08')
+
+-- insert patron record
+INSERT INTO patron(
+	first_name,
+	last_name,
+	email, 
+	created) 
+  VALUES ('Johan', 'Hellberg', 'johan@jhellberg.com', current_timestamp);
+
+-- insert loan record
+INSERT INTO loan(
+  patron_id,
+  book_id,
+  checkout_date,
+  return_date
+) VALUES(
+  '2',
+  '3',
+  current_date,
+  current_date + 14
+);
+
+-- SELECT * FROM loan;
+
+-- update all items where column is null
+-- UPDATE catalogue SET quantity = (trunc(random()*5)) WHERE quantity IS NULL;
