3bc65c5564f0ffc08bf8908c0bacc4e8f3b5be7c
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-11 15:17:32 +0200

    add quantity to bookModel

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,8 +1,9 @@
 module.exports = class book {
-  constructor(author, title, genre, published) {
+  constructor(author, title, genre, published, quantity) {
     this.author = author;
     this.title = title;
     this.genre = genre;
     this.published = published;
+    this.quantity = quantity;
   }
 }
